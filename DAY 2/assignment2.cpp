ce58ca94295bf42d21f695fe00c902bf91cd349a
Hari Raja Prabhu P <109938413+hariprabhu571@users.noreply.github.com>
2023-10-05 08:19:24 +0530

    Update assignment2.cpp

--- DAY 2/assignment2.cpp
+++ DAY 2/assignment2.cpp
@@ -6,20 +6,19 @@ public:
     int children;
     int buses;
 
-    // Constructor to initialize children and buses
+    
     Calc(int c, int b) : children(c), buses(b) {}
 
-    // A method to calculate and display the total
+   
     void mymethod() {
         cout << "Total children: " << children * buses << endl;
     }
 };
 
 int main() {
-    // Create an object of the Calc class and initialize it with values
+   
     Calc number(48, 3);
     
-    // Call the method to calculate and display the total
     number.mymethod();
 
     return 0;
